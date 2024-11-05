9d0812e8cedfa0ea4045491df8c2a85a0211986c
NAD LAB <aimforbigfoot@gmail.com>
2024-11-05 06:27:32 -0500

    start of data generation for my registered report

---
+++ registeredReportAssignment/dataGenerationForHaptics.R
@@ -0,0 +1,14 @@
+# # WTF am i trying to do?
+# - I am trying to generate data for my experiment that i can analyze later
+#   using various
+#   - parametric statistics (sd, t-test, anova, general linear mixed model)
+#   - a power analysis,
+
+
+# - generate data for each person, so i need parameters of  
+#   - person, mean offset on the x and y, the standard deviations of them 
+#   - their height, their wingspan (for a general linear mixed model)
+#   - id need angles of the perosons hand and joints in space basically
+#   - 
+#   - 
+#   - 
